1786a34a03f5a2e4fb6c48d07ce3eb02b7553f36
Marc McKernan <marc.mckernan@gmail.com>
2019-06-06 15:57:56 -0400

    command line arg

--- cmd/maze/maze.go
+++ cmd/maze/maze.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/smuggy/go-maze/internal/basics"
 	"github.com/smuggy/go-maze/internal/builders"
@@ -9,50 +10,76 @@ import (
 	"strconv"
 )
 
+var builderAlgorithm = builders.AldousAlgo
+var printFullMaze = false
+var printEmptyMaze = false
+
 func sum(x int, y int) int {
 	return x + y
 }
 
 func usage(programName string) {
-	fmt.Printf("Usage: %s rows cols\n\n", programName)
+	fmt.Printf("Usage: %s [--algo=algorithm] [--empty] [--full] rows cols\n\n", programName)
 	os.Exit(1)
+}
+
+func processArgs() (int8, int8) {
+	var rows int8 = 5
+	var cols int8 = 5
+	var progName = os.Args[0]
+
+	algo := flag.String("algo", "aldous", "Maze building algorithm")
+	flag.BoolVar(&printEmptyMaze, "empty", false, "Print empty maze")
+	flag.BoolVar(&printFullMaze, "full", false, "Print full maze")
+	flag.Parse()
+
+	switch *algo {
+	case "aldous":
+		builderAlgorithm = builders.AldousAlgo
+	case "binary":
+		builderAlgorithm = builders.BinaryTreeAlgo
+	case "sidewinder":
+		builderAlgorithm = builders.SidewinderAlgo
+	default:
+		builderAlgorithm = builders.SidewinderAlgo
+	}
+
+	args := flag.Args()
+	if len(args) == 2 {
+		r, e := strconv.ParseInt(args[0], 10, 8)
+		if e == nil {
+			rows = int8(r)
+		} else {
+			usage(progName)
+		}
+		c, e := strconv.ParseInt(args[1], 10, 8)
+		if e == nil {
+			cols = int8(c)
+		} else {
+			usage(progName)
+		}
+	} else if len(args) != 0 {
+		usage(progName)
+	}
+	return rows, cols
 }
 
 func main() {
 	fmt.Print("Starting application...\n\n")
-	var rows int8 = 5
-	var cols int8 = 5
-	if len(os.Args) == 3 {
-		r, e := strconv.ParseInt(os.Args[1], 10, 8)
-		if e == nil {
-			rows = int8(r)
-		} else {
-			usage(os.Args[0])
-		}
-		c, e := strconv.ParseInt(os.Args[2], 10, 8)
-		if e == nil {
-			cols = int8(c)
-		} else {
-			usage(os.Args[0])
-		}
-	} else if len(os.Args) != 1 {
-		usage(os.Args[0])
-	}
-	//	var d = basics.BuildCell(2, 1)
-	//	fmt.Println(d)
+	rows, cols := processArgs()
 	var g = basics.BuildGrid(rows, cols)
-	var b = createBuilder(builders.AldousAlgo)
+	var b = createBuilder(builderAlgorithm)
 	b.Build(g)
-	fmt.Print(g.PrintGrid())
-	//	fmt.Println("\nNext builder...")
-	//	var g2 = basics.BuildGrid(4, 5)
-	//	builders.Sidewinder(g2)
-	//	fmt.Println(g2.PrintGrid())
+	if printEmptyMaze {
+		fmt.Print(g.PrintGrid())
+	}
 	fmt.Println("\nApply solver to grid...")
 
 	var solution solvers.Solution = solvers.DijkstraSolver(g.GetCell(0, 0))
-	solution.ApplyDistances()
-	fmt.Println(g.PrintGrid())
+	if printFullMaze {
+		solution.ApplyDistances()
+		fmt.Println(g.PrintGrid())
+	}
 
 	g.ClearValues()
 	fmt.Println("Find maximum path...")
